c5774543e2e6cf5b348320cdbb3158fef5dfd2c7
github username <github email address>
2019-09-27 12:36:27 -0400

    set up state value and bind setState to event handler onChange'

--- src/components/headerForm.js
+++ src/components/headerForm.js
@@ -2,9 +2,23 @@ import React, {Component} from 'react';
 
 class HeaderForm extends Component {
 
+  state = {
+    header: ''
+  }
+
+  onChange =(e) => {
+    this.setState( {
+      header: e.target.value
+    })
+  }
+  
   render() {
     return (
-      <div>This is the headerForm</div>
+      <form>
+        <h4>Please add contents to change header</h4>
+        <input type='text' placeholder='Change header content' value={this.state.header} onChange = {this.onChange}/>
+        <p>State header value: {this.state.header}</p>
+      </form>
     )
   }
 }
